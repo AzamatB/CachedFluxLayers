e3f602bf48122073c9f4f2a731ecb1a216cd5105
azamat <aberdysh@gmail.com>
2020-03-31 18:06:51 +0600

    fix test failures on GPU

--- test.jl
+++ test.jl
@@ -1,4 +1,3 @@
-###
 using BenchmarkTools
 using CuArrays
 using Flux
@@ -53,28 +52,35 @@ end
 gsf[mf.W] == gs[m.W]
 gsf[mf.b] == gs[m.b]
 
+###
 function reorder(x::AbstractMatrix)
-    o = size(x,1) ÷ 4
-    [x[1:(2o),:];
-     x[(3o+1):end,:];
-     x[(2o+1):(3o),:]]
+   o = size(x,1) ÷ 4
+   [x[1:(2o),:];
+    x[(3o+1):end,:];
+    x[(2o+1):(3o),:]]
 end
 function reorder(x::AbstractVector)
-    o = size(x,1) ÷ 4
-    [x[1:(2o)];
-     x[(3o+1):end];
-     x[(2o+1):(3o)]]
+   o = size(x,1) ÷ 4
+   [x[1:(2o)];
+    x[(3o+1):end];
+    x[(2o+1):(3o)]]
 end
 
 in, out, batchsize = 3, 5, 7
-x = rand(Float32, in, batchsize)
-m = Flux.LSTM(in, out); m(x)
-(h, c) = m.state
-m = CachedLSTMCell(in, out; batchsize=3batchsize)
-mf = Flux.LSTMCell(reorder(m.Wi), reorder(m.Wh), reorder(m.b), zero(m.b), zero(m.b))
+x = rand(Float32, in, batchsize) |> gpu
+mf = Flux.LSTM(in, out) |> gpu; mf(x)
+(h, c) = mf.state
+(h1, c1), h1 = mf.cell((h, c), x)
 
-(h1, c1), h1 = m((copy(h), copy(c)), x)
-(h2, c2), h2 = mf((copy(h), copy(c)), x)
+m = CachedLSTMCell(in, out; batchsize=3batchsize) |> gpu
+m = CachedLSTMCell(reorder(mf.cell.Wi), reorder(mf.cell.Wh), reorder(mf.cell.b), m.cache) |> gpu
 
+(h2, c2), h2 = m((copy(h), copy(c)), x)
 h1 == h2
 c1 == c2
+
+h1 - h2
+c1 - c2
+
+h1 ≈ h2
+c1 ≈ c2
